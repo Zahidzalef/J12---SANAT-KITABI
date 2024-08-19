f733be98ab139ff3a068064ea17a951deb20fd94
Zahid İyigökler <iyigoklerzahid55@gmail.com>
2024-08-19 09:08:28 +0300

    File added and error.

---
+++ app/src/main/java/com/zahidiyigokler/a5sanatkitab/Art.java
@@ -0,0 +1,10 @@
+package com.zahidiyigokler.a5sanatkitab;
+
+public class Art {
+    String name;
+    int id;
+    public Art(String name,int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
---
+++ app/src/main/java/com/zahidiyigokler/a5sanatkitab/MainActivity.java
@@ -0,0 +1,96 @@
+package com.zahidiyigokler.a5sanatkitab;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.zahidiyigokler.a5sanatkitab.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
+import javax.xml.transform.Result;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    ArrayList<Art> artList;
+    ArtAdapter artAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        artList = new ArrayList<Art>();
+
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        artAdapter = new ArtAdapter(artList);
+        binding.recyclerView.setAdapter(artAdapter);
+
+        getData();
+
+    }
+
+    public void getData() {
+
+        try {
+            SQLiteDatabase database = this.openOrCreateDatabase("Arts",MODE_PRIVATE,null);
+
+            Cursor cursor = database.rawQuery("SELECT * FROM arts", null);
+            int nameIx = cursor.getColumnIndex("artname");
+            int idIx = cursor.getColumnIndex("id");
+
+            while (cursor.moveToNext()) {
+                String name = cursor.getString(nameIx);
+                int id = cursor.getInt(idIx);
+                Art art = new Art(name,id);
+                artList.add(art);
+            }
+            artAdapter.notifyDataSetChanged();
+
+            cursor.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflater
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_art,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.add_art_item) {
+            Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+            intent.putExtra("info","new");
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
